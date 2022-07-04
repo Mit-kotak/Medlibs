f344709ffef3ef08ffccdb650bd4742f2b798d64
Mit Kotak <mitkotak10@gmail.com>
2022-07-04 15:19:46 +0530

    project

---
+++ medlibs.py
@@ -0,0 +1,16 @@
+print("Love letter for He/She")
+name1 = input("write a name of person: ")
+adj = input("Adjective: ")
+vrb = input("Verb: ")
+part = input("Part of Body: ")
+num = input("number: ")
+noun = input("noun: ")
+adverb = input("Adverb: ")
+vrb2 = input("Verb: ")
+name2 = input("Your name: ")
+
+madlib = f"         Dear {name1},\n \
+           You are extremly {adj} and I {vrb} you! I want kiss your {part} {num} times.\nYou make my {noun} burn with desire. When I first saw you, I {adverb} stared at you and fell in love.\nWill you {vrb2} out with me? Don`t let your parents discourage you,\nFamilies are just jealous.\n \
+                    Yours forever, {name2}"
+
+print(madlib)
